f946912303b19c09f3624b95449d3bd6d3b9bf95
thaovycute <nhtv.thaovy@gmail.com>
2025-08-18 13:39:41 +0700

    yii2_contact

---
+++ yii2_contact/migrations/m250818_045529_create_contact_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%contact}}`.
+ */
+class m250818_045529_create_contact_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%contact}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'email' => $this->string()->notNull(),
+            'subject' => $this->string()->notNull(),
+            'body' => $this->text()->notNull(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%contact}}');
+    }
+}
---
+++ yii2_contact/models/Contact.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contact".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $subject
+ * @property string $body
+ * @property string|null $created_at
+ */
+class Contact extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contact';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'subject', 'body'], 'required'],
+            [['body'], 'string'],
+            [['created_at'], 'safe'],
+            [['name', 'email', 'subject'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'subject' => 'Subject',
+            'body' => 'body',
+            'created_at' => 'Created At',
+        ];
+    }
+
+}
